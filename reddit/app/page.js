7f5f99a8004a1a17f2c359038f113730e81c4376
kipkoech88 <hoseakipkoech88@gmail.com>
2023-12-26 21:53:34 +0300

    added icons

--- reddit/app/page.js
+++ reddit/app/page.js
@@ -2,13 +2,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Header from './components/Header'
 import Logo from './reddit-1.svg'
-import home from '../public/home.png'
+import home from '../public/home-icon-silhouette-svgrepo-com.svg'
 import messenger from '../public/messenger.png'
 import banana from '../public/banana.png'
 import advertising from '../public/advertising.png'
 import plus from '../public/plus.png'
 import trending from '../public/trending.png'
 import notification from '../public/bell.png'
+import down from '../public/down-arrow-svgrepo-com.svg'
+import profile from '../public/user-profile-svgrepo-com.svg'
 
 export default function Home() {
   return (
@@ -23,11 +25,13 @@ export default function Home() {
             <button className='w-full hover:border border-gray-500 rounded-sm'>
             <div className='flex justify-between   w-full border py-2 h-10 items-center'>
             <div className='flex space-x-4 mr-6'>
-              {/* <Image src={home} alt='Reddit' width={20}/> */}
+              <Image src={home} alt='Reddit' width={20}/>
               <div className='py-2 order-1'>Home</div>
             </div>
             <div>
-                <div className='py-2'>Arr</div>
+                <div className='py-2 px-2'>
+                  <Image src={down} width={20}/>
+                </div>
             </div>
           </div>
             </button>
@@ -36,7 +40,7 @@ export default function Home() {
         <div className='flex w-1/3 my-2'>
             <input type='text' className='w-full peer rounded-full py-2 px-4 border border-gray text-sm hover:border-blue-500' placeholder='Search Reddit'></input>
         </div>
-        <div className='flex w-1/3 justify-between space-x-4 my-5  items-centre'>
+        <div className='flex w-1/3 justify-between space-x-4   items-centre'>
           <div className='flex justify-between w-1/2'>
             <div>
             </div>
@@ -64,15 +68,15 @@ export default function Home() {
               <div className='flex justify-between spce-x-8'>
                 <div className='w-2/3 flex space-x-2 mr-6'>
                   <div className='w-1/3'>
-                    Img
+                    <Image src={profile} width={70}/>
                   </div>
                   <div>
                     <h4 className='text-xs'>Academia_wing</h4>
                     <h6 className='text-xs'>1 karma</h6>
                   </div>
                 </div>
-                <div className=''>
-                  arr
+                <div className='px-2'>
+                  <Image src={down} width={20} className='py-2'/>
                 </div>
               </div>
             </button>
